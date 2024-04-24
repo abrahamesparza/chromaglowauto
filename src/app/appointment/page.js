aa147b719583c8a9243344e4cea5e9d2f22e95f1
Abraham Esparza <abrahamesparza.dev@gmail.com>
2024-04-24 16:47:12 -0700

    Delete unused files, add new page

---
+++ src/app/appointment/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function Appointment() {
+    return (
+        <div>
+            <h1>Appointment page</h1>
+            <h2>Return <Link href={'/'}>Home</Link></h2>
+        </div>
+    )
+}
